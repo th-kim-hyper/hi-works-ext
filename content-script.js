a0e44b386737904c41f12ffa17580f4839c3db32
태현 김 <th.kim@hyperinfo.co.kr>
2025-02-04 19:49:50 +0900

    download 추가

--- content-script.js
+++ content-script.js
@@ -30,6 +30,18 @@ function dataUrlToBlob(dataUrl) {
   return new Blob([ab], { type: mimeString });
 }
 
+function downloadDataUrl(dataUrl, filename) {
+  const blob = dataUrlToBlob(dataUrl);
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
 function createElementFromHTML(htmlString) {
   const template = document.createElement('template');
   template.innerHTML = htmlString.trim();
